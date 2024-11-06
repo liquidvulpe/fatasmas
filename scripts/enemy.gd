5a1a91f2cddd9b109bce849bcf26b0f4147e44f0
Charlie <charlie94sa@gmail.com>
2024-11-06 20:42:06 -0300

    comportamento do inimmigo

---
+++ scripts/enemy.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var target
+const SPEED = 0.75
+var curr_area
+
+func _on_area_2d_area_entered(area):
+	if(area.is_in_group("area")):
+		curr_area = area
+
+func _process(delta):
+	target = curr_area.player
+
+func _physics_process(delta):
+	if(curr_area != null and target != null):
+		position += sign(target.position - position)*SPEED
+		move_and_slide()
+
+
